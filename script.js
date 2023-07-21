f29c563025f4bc524ff5f5cf19870ceadbf1bf4b
gaurav-3232 <gauravmalik3232@gmail.com>
2023-07-21 18:14:40 +0530

    project has completed

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ const toggle = () => {
     }
 }
 
-function carousel() {
-    let carouselSlider = document.querySelector(".carousel-slider")
-    let list = document.querySelector(".carousel-list")
-    let item = document.querySelector(".carousel-item")
+function carousal() {
+    let carousalSlider = document.querySelector(".carousal-slider")
+    let list = document.querySelector(".carousal-list")
+    let item = document.querySelector(".carousal-item")
     let list2
 
     const speed = 1
@@ -23,7 +23,7 @@ function carousel() {
 
     function clone() {
         list2 = list.cloneNode(true)
-        carouselSlider.appendChild("list2")
+        carousalSlider.appendChild(list2)
         list2.style.left = `${width}px`
     }
 
@@ -63,8 +63,8 @@ function carousel() {
     let a = setInterval(moveFirst, 10)
     let b = setInterval(moveSecond, 1)
 
-    carouselSlider.addEventListener("mouseenter", hover)
-    carouselSlider.addEventListener("mouseenter", unhover)
+    carousalSlider.addEventListener("mouseenter", hover)
+    carousalSlider.addEventListener("mouseenter", unhover)
 
 }
-carousel()
+carousal()
